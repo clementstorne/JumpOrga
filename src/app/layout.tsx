5eeeb8adacc4b6833fbca38f78117b0e008454d4
Clément Storne <clementstorne@gmail.com>
2024-06-11 19:11:21 +0200

    Navbar
    - Integrated shadcn/ui Button component.
    - Added a logo that links to the homepage.
    - Created a responsive burger button for mobile view.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
